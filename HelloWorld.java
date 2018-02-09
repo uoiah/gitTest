25a44d93906ec447245415dc61988876daee0e55
uoiah <uoiah@hotmail.com>
2018-02-09 17:43:55 +0800

    b2nd branch's first commit...

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
   public static void main(String[] args){
     System.out.println("Hello World, I'm Java!");
+    System.out.println("");
+    System.out.println("");
   }
 }
